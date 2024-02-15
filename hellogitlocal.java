b11f371976568d62c0bd714b9ace4120bf1e6dd0
Guillermo <guillermo68@gmail.com>
2024-02-15 10:30:59 +0100

    tercera version de hellogitlocal.java creada desde main(local)

--- hellogitlocal.java
+++ hellogitlocal.java
@@ -5,6 +5,10 @@ Contenido del fichero
 Linea1 escrito desde local
 Linea2 escrito desde local
 Linea3 escrito desde local
+Linea4 escrito desde local
+Linea5 escrito desde local
+Linea6 escrito desde local
+
 
 
 
